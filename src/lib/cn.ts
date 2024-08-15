263048c70ba905d9efc4fad0259e7a5da5cc25f4
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2024-08-15 08:08:43 +0200

    Linting fix

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export type CN<T> = T & { className: string }
+export type CN<T> = T & { className?: string }
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
--- src/view/ui/select.tsx
+++ src/view/ui/select.tsx
@@ -1,4 +1,4 @@
-import { cn } from '@/lib/cn'
+import { CN, cn } from '@/lib/cn'
 import * as SelectPrimitive from '@radix-ui/react-select'
 import { Check, ChevronDown, ChevronUp } from 'lucide-react'
 import * as React from 'react'
@@ -11,7 +11,7 @@ const SelectValue = SelectPrimitive.Value
 
 const SelectTrigger = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
+  CN<React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>>
 >(({ className, children, ...props }, ref) => (
   <SelectPrimitive.Trigger
     ref={ref}
@@ -31,7 +31,7 @@ SelectTrigger.displayName = SelectPrimitive.Trigger.displayName
 
 const SelectScrollUpButton = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.ScrollUpButton>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollUpButton>
+  CN<React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollUpButton>>
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.ScrollUpButton
     ref={ref}
@@ -48,7 +48,7 @@ SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName
 
 const SelectScrollDownButton = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.ScrollDownButton>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollDownButton>
+  CN<React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollDownButton>>
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.ScrollDownButton
     ref={ref}
@@ -66,7 +66,7 @@ SelectScrollDownButton.displayName =
 
 const SelectContent = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
+  CN<React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>>
 >(({ className, children, position = 'popper', ...props }, ref) => (
   <SelectPrimitive.Portal>
     <SelectPrimitive.Content
@@ -98,7 +98,7 @@ SelectContent.displayName = SelectPrimitive.Content.displayName
 
 const SelectLabel = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Label>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Label>
+  CN<React.ComponentPropsWithoutRef<typeof SelectPrimitive.Label>>
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.Label
     ref={ref}
@@ -110,7 +110,7 @@ SelectLabel.displayName = SelectPrimitive.Label.displayName
 
 const SelectItem = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
+  CN<React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>>
 >(({ className, children, ...props }, ref) => (
   <SelectPrimitive.Item
     ref={ref}
@@ -133,7 +133,7 @@ SelectItem.displayName = SelectPrimitive.Item.displayName
 
 const SelectSeparator = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Separator>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Separator>
+  CN<React.ComponentPropsWithoutRef<typeof SelectPrimitive.Separator>>
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.Separator
     ref={ref}
