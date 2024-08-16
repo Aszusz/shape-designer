bc8e6e83e5097eff3bc5406e41ee64106ee34a6f
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2024-08-16 15:15:36 +0200

    Working devtools

--- src/shell/store.ts
+++ src/shell/store.ts
@@ -4,6 +4,7 @@ import { BoundingBox, Point, Size } from '../model/geometry'
 import { ReadonlyOrderedRecord } from '../model/readonlyOrderedRecord'
 import { SelectionToolMode, ToolType } from '../model/tools'
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
 
 interface StoreState {
   // State
@@ -33,71 +34,84 @@ interface StoreState {
   deleteSelectedShapes: () => void
 }
 
-export const useStore = create<StoreState>((set, get) => ({
-  // State
-  ...core.initialState,
+export const useStore = create<StoreState>()(
+  devtools((set, get) => ({
+    // State
+    ...core.initialState,
 
-  // Computed selectors
-  getCanvasBorderSize: () => core.getBorderSize(get()),
-  getToolPreview: () => core.getPreview(get()),
-  getShapeIds: () => get().shapes.getOrder(),
-  getShape: (shapeId: string) => get().shapes.get(shapeId),
-  getSelectedShapes: () => core.getSelectedShapes(get()),
+    // Computed selectors
+    getCanvasBorderSize: () => core.getBorderSize(get()),
+    getToolPreview: () => core.getPreview(get()),
+    getShapeIds: () => get().shapes.getOrder(),
+    getShape: (shapeId: string) => get().shapes.get(shapeId),
+    getSelectedShapes: () => core.getSelectedShapes(get()),
 
-  // Actions
-  setCanvasSize: (size: Size) => {
-    set(state => ({ ...core.setCanvasSize(state, size) })),
-      undefined,
-      'setCanvasSize'
-  },
+    // Actions
+    setCanvasSize: (size: Size) => {
+      set(state => ({ ...core.setCanvasSize(state, size) })),
+        undefined,
+        'setCanvasSize'
+    },
 
-  setTool: (tool: ToolType) => {
-    set(state => ({ ...core.setTool(state, tool) })), undefined, 'setTool'
-  },
+    setTool: (tool: ToolType) => {
+      set(state => ({ ...core.setTool(state, tool) }), undefined, 'setTool')
+    },
 
-  onMouseMove: (mousePosition: Point) => {
-    set(state => ({ ...core.onMouseMove(state, mousePosition) })),
-      undefined,
-      'onMouseMove'
-  },
+    onMouseMove: (mousePosition: Point) => {
+      set(state => ({ ...core.onMouseMove(state, mousePosition) }))
+      // Skip logging for this action
+    },
 
-  onMouseDown: (mousePosition: Point) => {
-    set(state => ({ ...core.onMouseDown(state, mousePosition) })),
-      undefined,
-      'onMouseDown'
-  },
+    onMouseDown: (mousePosition: Point) => {
+      set(
+        state => ({ ...core.onMouseDown(state, mousePosition) }),
+        undefined,
+        'onMouseDown'
+      )
+    },
 
-  onMouseUp: (mode: SelectionToolMode) => {
-    set(state => ({ ...core.onMouseUp(state, mode) })), undefined, 'onMouseUp'
-  },
+    onMouseUp: (mode: SelectionToolMode) => {
+      set(state => ({ ...core.onMouseUp(state, mode) }), undefined, 'onMouseUp')
+    },
 
-  selectShape: (shapeId: string) => {
-    set(state => ({ ...core.selectShape(state, shapeId) })),
-      undefined,
-      'selectShape'
-  },
+    selectShape: (shapeId: string) => {
+      set(
+        state => ({ ...core.selectShape(state, shapeId) }),
+        undefined,
+        'selectShape'
+      )
+    },
 
-  toggleSelected: (shapeId: string) => {
-    set(state => ({ ...core.toggleSelected(state, shapeId) })),
-      undefined,
-      'toggleSelected'
-  },
+    toggleSelected: (shapeId: string) => {
+      set(
+        state => ({ ...core.toggleSelected(state, shapeId) }),
+        undefined,
+        'toggleSelected'
+      )
+    },
 
-  updateShape: (shape: core.Shape) => {
-    set(state => ({ ...core.updateShape(state, shape) })),
-      undefined,
-      'updateShape'
-  },
+    updateShape: (shape: core.Shape) => {
+      set(
+        state => ({ ...core.updateShape(state, shape) }),
+        undefined,
+        'updateShape'
+      )
+    },
 
-  setSnapToGridSetting: (snapToGrid: boolean) => {
-    set(state => ({ ...core.setSnapToGridSetting(state, snapToGrid) })),
-      undefined,
-      'setSnapToGridSetting'
-  },
+    setSnapToGridSetting: (snapToGrid: boolean) => {
+      set(
+        state => ({ ...core.setSnapToGridSetting(state, snapToGrid) }),
+        undefined,
+        'setSnapToGridSetting'
+      )
+    },
 
-  deleteSelectedShapes: () => {
-    set(state => ({ ...core.deleteSelectedShapes(state) })),
-      undefined,
-      'deleteSelectedShapes'
-  }
-}))
+    deleteSelectedShapes: () => {
+      set(
+        state => ({ ...core.deleteSelectedShapes(state) }),
+        undefined,
+        'deleteSelectedShapes'
+      )
+    }
+  }))
+)
